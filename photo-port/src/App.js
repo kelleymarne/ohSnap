746e13749089ccdd6ad72791634a8da5109d09a0
Kelley Worthington <kelleymarne@gmail.com>
2021-09-01 12:50:21 -0600

    added photolist and test suites

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery'
+import Gallery from './components/Gallery';
 import './App.css';
-
 
 function App() {
   const [categories] = useState([
@@ -26,10 +25,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
